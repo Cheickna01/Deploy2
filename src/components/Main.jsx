17f4c7dfab26978aca6556e4d8b3c6e20553e0a6
Cheickna01 <diabyhamala0@gmail.com>
2024-07-04 22:17:07 +0100

    modif

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,16 @@
-import {Route,Routes } from 'react-router-dom';
-import App from '../App';
-import Info from './Info';  
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "../App";
+import Info from "./Info";
 
 export default function Main() {
   return (
     <>
+      <BrowserRouter>
         <Routes>
-            <Route path='/' element={<App/>}/>
-            <Route path='/info' element={<Info/>}/>
+          <Route path="/" element={<App />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
