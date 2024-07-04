2b48630226370ab430f1fff0293a898b5bca46e7
Cheickna01 <diabyhamala0@gmail.com>
2024-07-04 22:08:35 +0100

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import {Route,Routes } from 'react-router-dom';
 import App from '../App';
+import Info from './Info';  
 
 export default function Main() {
   return (
     <>
         <Routes>
-            <Route path='/info' element={<App/>}/>
+            <Route path='/' element={<App/>}/>
+            <Route path='/info' element={<Info/>}/>
         </Routes>
     </>
   )
